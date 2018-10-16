ca5177d7ef90babf0dd59dd524eea9752d5562fa
fernando <ejemplo696@gmail.com>
2018-10-16 17:25:59 +0200

    Menu de selecciónde personaje

---
+++ Assets/PlayerSelected.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class PlayerSelected : MonoBehaviour {
+
+	public PlayerActions Actions { get; set; }
+}
