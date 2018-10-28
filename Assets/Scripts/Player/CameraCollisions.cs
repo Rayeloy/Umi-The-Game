b186beb5d44b1de2f45cc52299e0be1a037bf60f
Eloy <rayeloy@hotmail.com>
2018-10-28 16:30:44 +0100

    update eloy

--- Assets/Scripts/Player/CameraCollisions.cs
+++ Assets/Scripts/Player/CameraCollisions.cs
@@ -51,4 +51,72 @@ public class CameraCollisions : MonoBehaviour {
         }
         myCamController.targetMyCamPos = dollyDir * distance;
     }
+
+    bool shaking, smoothShakeStart_End;
+    float TimeShaking;
+    float MaxTimeShaking;
+    float ShakingSize;
+    float NextShakeTime;
+    float MaxNextShakeTime;
+    Vector2 shakedPos;
+    public void shakeCameraTrial(float time)
+    {
+        StartShakeCamera(time, 0.5f, 0.08f, true);
+    }
+    public void StartShakeCamera(float time, float size = 0.3f, float shakeFreq = 0.2f, bool _smoothShakeStart_End = true)
+    {
+        shaking = true;
+        smoothShakeStart_End = _smoothShakeStart_End;
+        shakedPos = Vector2.zero;
+        TimeShaking = 0;
+        MaxTimeShaking = time;
+        ShakingSize = size;
+        NextShakeTime = MaxNextShakeTime + 1;
+    }
+    void ShakeCamera()
+    {
+        if (shaking)
+        {
+            //Debug.Log("SHAKING CAMERA");
+            if (TimeShaking >= MaxTimeShaking)
+            {
+                shaking = false;
+            }
+            float actShakingSize = ShakingSize;
+            //smoothShake
+            if (smoothShakeStart_End)
+            {
+                if (TimeShaking < MaxTimeShaking / 5)//beggining
+                {
+                    float prog = TimeShaking / (MaxTimeShaking / 5);
+                    actShakingSize = Mathf.Lerp(0, ShakingSize, prog);
+                }
+                else if (TimeShaking >= (MaxTimeShaking / 5) && TimeShaking <= (MaxTimeShaking - (MaxTimeShaking / 5)))
+                {
+                    actShakingSize = ShakingSize;
+                }
+                else if (TimeShaking > (MaxTimeShaking - (MaxTimeShaking / 5)))
+                {
+                    float timeStartEnd = (MaxTimeShaking - (MaxTimeShaking / 5));
+                    float prog = (TimeShaking - timeStartEnd) / (MaxTimeShaking - timeStartEnd);
+                    actShakingSize = Mathf.Lerp(ShakingSize, 0, prog);
+                }
+            }
+            else
+            {
+                actShakingSize = ShakingSize;
+            }
+
+            if (NextShakeTime >= MaxNextShakeTime)
+            {
+                NextShakeTime = 0;
+                float shakedX = Random.Range(-actShakingSize, actShakingSize);
+                float shakedY = Random.Range(-actShakingSize, actShakingSize);
+                shakedPos = new Vector2(shakedX, shakedY);
+            }
+            //focusPosition = new Vector2(cameraTarget.x + shakedPos.x, cameraTarget.y + shakedPos.y);
+            TimeShaking += Time.deltaTime;
+            NextShakeTime += Time.deltaTime;
+        }
+    }
 }
