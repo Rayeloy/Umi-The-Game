907d717eab54fffdadf842fc632b156762e8d7ed
fernando <ejemplo696@gmail.com>
2018-11-16 22:29:47 +0100

    Bases

---
+++ Assets/Scripts/UI/ButtonPlayAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPlayAnimation : MonoBehaviour
+{
+	[Header("Referencias")]
+	public Animator animator;
+
+	public void Play (string name){
+		animator.SetTrigger(name);
+	}
+}
