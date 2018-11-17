6712c3fefea8d645c5d7dd5147b6ef131bb80998
Eloy <rayeloy@hotmail.com>
2018-11-17 18:31:57 +0100

    Collider radius changes dinamicly when moving, some bug fixes to flag mode /timer

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 using TMPro;
 
 public class ScoreManager : MonoBehaviour {
-
+    public static ScoreManager instance;
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Awake()
+    {
+        instance = this;
+    }
 
     [Tooltip("Teimpo de juego en segundos")]
     public float Tiempo = 120;
@@ -21,11 +17,12 @@ public class ScoreManager : MonoBehaviour {
 	[HideInInspector]
 	public bool End = false;
 
-	public int _blueTeamScore;
+    public int maxScore;
+    public int _blueTeamScore;
 	public int _redTeamScore;
 
+
 	[Header("Referencias")]
-	public GameController gameController;
 	public TextMeshProUGUI[] blueTeamScore_Text;
 	public TextMeshProUGUI[] redTeamScore_Text;
 	public TextMeshProUGUI[] time_Text;
@@ -55,28 +52,43 @@ public class ScoreManager : MonoBehaviour {
 		Debug.Log(elTiempo);
 
         if (Tiempo <= 0){
-            gameController.playing = false;
-
-			if (_blueTeamScore > _redTeamScore)
-				gameController.winnerTeam = Team.blue;
-			else
-				gameController.winnerTeam = Team.red;
-			gameController.SwitchGameOverMenu();
+            Team winner;
+            if (_blueTeamScore == _redTeamScore)
+            {
+                //PROGRAMAR EL CASO DE QUE AMBOS ACABEN CON EL MISMO SCORE
+            }
+            else
+            {
+                winner = _blueTeamScore > _redTeamScore ? Team.blue : Team.red;
+                GameController.instance.GameOver(winner);
+            }
         }
     }
 
-	public void ScorePoint (Team _winnerTeam){
-		switch (_winnerTeam)
+	public void ScorePoint (Team scoringTeam){
+        switch (scoringTeam)
         {
-            case Team.blue :
-				_blueTeamScore ++;
-				foreach(TextMeshProUGUI tM in blueTeamScore_Text)
-					tM.text = _blueTeamScore.ToString();
+            case Team.blue:
+                _blueTeamScore++;
+                foreach (TextMeshProUGUI tM in blueTeamScore_Text)
+                {
+                    tM.text = _blueTeamScore.ToString();
+                }
+                if (_blueTeamScore >= maxScore)
+                {
+                    GameController.instance.GameOver(scoringTeam);
+                }
                 break;
-            case Team.red :
-				_redTeamScore ++;
-				foreach(TextMeshProUGUI tM in redTeamScore_Text)
-					tM.text = _redTeamScore.ToString();
+            case Team.red:
+                _redTeamScore++;
+                foreach (TextMeshProUGUI tM in redTeamScore_Text)
+                {
+                    tM.text = _redTeamScore.ToString();
+                }
+                if (_redTeamScore >= maxScore)
+                {
+                    GameController.instance.GameOver(scoringTeam);
+                }
                 break;
         }
 	}
