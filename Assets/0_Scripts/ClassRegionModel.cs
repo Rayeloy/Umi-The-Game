e564698198f83c990233279c3e0a339d6dec6534
KataPUMB <kaylakmail@gmail.com>
2019-01-27 21:01:52 +0100

    Añadida una clase modelo dividida en regiones para ogranizar los scripts

---
+++ Assets/0_Scripts/ClassRegionModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region ----[ PUBLIC ENUMS ]----
+#endregion
+
+public class RegionClassModel : MonoBehaviour
+{
+    #region ----[ HEADER REFERENCES ]----
+    #endregion
+
+    #region ----[ PROPERTIES ]----
+    #endregion
+
+    #region ----[ VARIABLES ]----
+    #endregion
+
+    #region ----[ MONOBEHAVIOUR FUNCTIONS ]----
+    #endregion
+
+    #region ----[ PRIVATE FUNCTIONS ]----
+    #endregion
+
+    #region ----[ PUBLIC FUNCTIONS ]----
+    #endregion
+
+    #region ----[ PUN CALLBACKS ]----
+    #endregion
+
+    #region ----[ RPC ]----
+    #endregion
+
+    #region ----[ NETWORK FUNCTIONS ]----
+    #endregion
+
+    #region ----[ IPUNOBSERVABLE ]----
+    #endregion
+}
