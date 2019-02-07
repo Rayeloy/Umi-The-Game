7182e143e1fa7d75af21e8be2ff8e95737421969
fernando <ejemplo696@gmail.com>
2019-02-07 09:08:56 +0100

    Corrección del error de la seleccion de equipo UMI ahora con menos fantasmas

---
+++ Assets/0_Scripts/UI/ButtonDisableObj.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class ButtonDisableObj : MonoBehaviour
+{
+   	public GameObject [] toDisable;
+	public PostProcessingBehaviour otherProfile;
+
+	public void DisableObjects (){
+		if (toDisable.Length < 0)
+			return;
+
+		for (int i = 0; i < toDisable.Length; i++){
+			toDisable[i].SetActive(false);
+		}
+	}
+
+	public void DisablePostprocesing()
+	{
+		otherProfile.enabled = false;
+	}
+}
