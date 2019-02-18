653578579a56a0f34d22304be5608712c082d193
KataPUMB <kaylakmail@gmail.com>
2019-02-18 17:40:16 +0100

    Con este cambio no debería tirarle error a gonzalo en el tutorial :). Actualizado el projectversion para que esté con la última versión de Unity y no tire error cada vez que se abre el proyecto

--- Assets/0_Scripts/Player/PlayerMovement.cs
+++ Assets/0_Scripts/Player/PlayerMovement.cs
@@ -252,9 +252,12 @@ public class PlayerMovement : MonoBehaviourPunCallbacks
 
         // #Important
         // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
-        if (photonView.IsMine)
+        if (online)
         {
-            PlayerMovement.LocalPlayerInstance = this.gameObject;
+            if (photonView.IsMine)
+            {
+                PlayerMovement.LocalPlayerInstance = this.gameObject;
+            }
         }
         // #Critical
         // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
