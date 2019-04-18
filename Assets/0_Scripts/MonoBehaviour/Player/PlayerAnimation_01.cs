a123e3e43b67a29afb90f8dc7eec722f361171e0
Clekio <alvaro.ortuno.m@gmail.com>
2019-04-18 19:27:07 +0200

    Nombre de variables cambiado, falta añadir las funciones nuevas

--- Assets/0_Scripts/MonoBehaviour/Player/PlayerAnimation_01.cs
+++ Assets/0_Scripts/MonoBehaviour/Player/PlayerAnimation_01.cs
@@ -11,27 +11,110 @@ public class PlayerAnimation_01 : MonoBehaviour
 
     [Header("Animator Variables")]
     AnimatorStateInfo stateInfo;
-    int jumpHash = Animator.StringToHash("Jump");
-    int jumpingHash = Animator.StringToHash("Jumping");
-    bool jumpingValue;
 
-    int landHash = Animator.StringToHash("Land");
-    bool landBool;
 
-    int jumpStateHash = Animator.StringToHash("Ascender");
-    int swimmingIdleHash = Animator.StringToHash("SwimmingIdle");
-    bool swimmingIdle;
+
+    int idleHash = Animator.StringToHash("Idle_01");
+    bool idle_01;
+
+    int pointScoreHash = Animator.StringToHash("PointScore");
+    bool pointScore;
+
+    int groundHash = Animator.StringToHash("Ground");
+    bool ground;
+
+    int walkHash = Animator.StringToHash("Walk");
+    bool walk;
+
+    int runHash = Animator.StringToHash("Run");
+    bool run;
+
+    int rollHash = Animator.StringToHash("Roll");
+    bool roll;
+
+    int airHash = Animator.StringToHash("Air");
+    bool air;
+
+    int fallingHash = Animator.StringToHash("Falling");
+    bool falling;
+
+    int startJumpHash = Animator.StringToHash("StartJump");
+    bool startJump;
+
+    int toGroundHash = Animator.StringToHash("ToGround");
+    bool toGround;
+
+    int toWaterHash = Animator.StringToHash("ToWater");
+    bool toWater;
+
+    int waterHash = Animator.StringToHash("Water");
+    bool water;
+
+    int idleWHash = Animator.StringToHash("IdleW");
+    bool idleW;
+
     int swimmingHash = Animator.StringToHash("Swimming");
     bool swimming;
-    int runningHash = Animator.StringToHash("Running");
-    bool runningValue;
-    int basicSwingHash = Animator.StringToHash("BasicSwing");
-    bool basicSwingValue;
-    int endGameHash = Animator.StringToHash("EndGame");
+
+    int noControlHash = Animator.StringToHash("NoControl");
+    bool noControl;
+
+    int dashHash = Animator.StringToHash("Dash");
+    bool dash;
+
+    int hookedHash = Animator.StringToHash("Hooked");
+    bool hooked;
+
+    int frontHitHash = Animator.StringToHash("FrontHit");
+    bool frontHit;
+
+    int backHitHash = Animator.StringToHash("BackHit");
+    bool backHit;
+
+    int bounceHash = Animator.StringToHash("Bounce");
+    bool bounce;
+
+    int attackHash = Animator.StringToHash("Attack");
+    bool attack;
+
+    int q_TipHash = Animator.StringToHash("Q_Tip");
+    bool q_Tip;
+
+    int hammerHash = Animator.StringToHash("Hammer");
+    bool hammer;
+
+
+
+    //To be added all the combat bools
+
+
+
+    ////int jumpHash = Animator.StringToHash("Jump");
+    ////int jumpingHash = Animator.StringToHash("Jumping");
+    ////bool jumpingValue;
+
+    ////int landHash = Animator.StringToHash("Land");
+    ////bool landBool;
+
+    ////int jumpStateHash = Animator.StringToHash("Ascender");
+    ////int swimmingIdleHash = Animator.StringToHash("SwimmingIdle");
+    ////bool swimmingIdle;
+    ////int swimmingHash = Animator.StringToHash("Swimming");
+    ////bool swimming;
+    ////int runningHash = Animator.StringToHash("Running");
+    ////bool runningValue;
+    ////int basicSwingHash = Animator.StringToHash("BasicSwing");
+
+
+    ////bool basicSwingValue;
+    ////int endGameHash = Animator.StringToHash("EndGame");
+
+
     [Tooltip("Distance to floor at which the landing animation will start")]
     public float maxTimeToLand = 1;
+
     //-------------
-    bool jump;
+    ////bool jump;
 
     //Churros: Pos: -0.0086, 0.0097, 0.0068; Rot: 45.854,-163.913,-5.477; Scale: 1.599267,1.599267,1.599267
 
@@ -47,20 +130,30 @@ public class PlayerAnimation_01 : MonoBehaviour
         {
             if ((myPlayerMovement.gC as GameController_FlagMode).myScoreManager.End)
                 {
-                animator.SetBool(endGameHash, true);
+                ////animator.SetBool(endGameHash, true);
                 //ResetVariables();
+
+                // Water can´t be False
+
                 return;
             }
         }
 
         animator.SetFloat("HorizontalSpeed", myPlayerMovement.currentSpeed);//new Vector2 (playerMovement.currentVel.x, playerMovement.currentVel.z).magnitude);
         animator.SetFloat("VerticalSpeed", myPlayerMovement.currentVel.y);
+
+
+
         animator.SetBool("OnGround", myPlayerMovement.controller.collisions.below);
+
+
+
         if (myPlayerMovement.wallJumpAnim)
         {
             animator.SetTrigger("WallJump");
             myPlayerMovement.wallJumpAnim = false;
         }
+
         ResetVariables();
         ProcessVariableValues();
 
@@ -74,32 +167,32 @@ public class PlayerAnimation_01 : MonoBehaviour
 
     void ResetVariables()
     {
-        if (landBool && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1 && !animator.IsInTransition(0))
+        if (ground && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1 && !animator.IsInTransition(0))
         {
-            landBool = false;
-            animator.SetBool(landHash, landBool);
+            ground = false;
+            animator.SetBool(groundHash, ground);
         }
         if (swimming && !myPlayerMovement.inWater)
         {
             swimming = false;
             animator.SetBool(swimmingHash, swimming);
         }
-        if (swimmingIdle && !myPlayerMovement.inWater)
+        if (idleW && !myPlayerMovement.inWater)
         {
-            swimmingIdle = false;
-            animator.SetBool(swimmingIdleHash, swimmingIdle);
+            idleW = false;
+            animator.SetBool(idleWHash, idleW);
         }
-        if (runningValue && !(myPlayerMovement.currentSpeed != 0 && myPlayerMovement.controller.collisions.below))
+        if (run && !(myPlayerMovement.currentSpeed != 0 && myPlayerMovement.controller.collisions.below))
         {
-            runningValue = false;
-            animator.SetBool(runningHash, runningValue);
+            run = false;
+            animator.SetBool(runHash, run);
         }
-        //COMBAT ANIMATIONS
-        if (basicSwingValue && myPlayerCombat.attackStg == AttackStage.ready)
-        {
-            basicSwingValue = false;
-            animator.SetBool(basicSwingHash, basicSwingValue);
-        }
+        //////COMBAT ANIMATIONS
+        ////if (basicSwingValue && myPlayerCombat.attackStg == AttackStage.ready)
+        ////{
+        ////    basicSwingValue = false;
+        ////    animator.SetBool(basicSwingHash, basicSwingValue);
+        ////}
     }
 
     public void ProcessVariableValues()
@@ -110,52 +203,53 @@ public class PlayerAnimation_01 : MonoBehaviour
 
         if (myPlayerMovement.currentSpeed != 0 && myPlayerMovement.controller.collisions.below)
         {
-            runningValue = true;
-            animator.SetBool(runningHash, runningValue);
+            run = true;
+            animator.SetBool(runHash, run);
         }
 
-        if (jump || (!myPlayerMovement.controller.collisions.below && myPlayerMovement.controller.collisions.lastBelow))
+        if (startJump || (!myPlayerMovement.controller.collisions.below && myPlayerMovement.controller.collisions.lastBelow))
         {
-            if (jump)
+            if (startJump)
             {
-                jump = false;
-                animator.SetBool(jumpHash, jump);
+                startJump = false;
+                animator.SetBool(startJumpHash, startJump);
             }
             if (swimming)
             {
                 swimming = false;
                 animator.SetBool(swimmingHash, swimming);
             }
-            if (landBool)
+            if (ground)
             {
-                landBool = false;
-                animator.SetBool(landHash, landBool);
+                ground = false;
+                animator.SetBool(groundHash, ground);
             }
-            jumpingValue = true;
-            animator.SetBool(jumpingHash, jumpingValue);
+
+            falling = true;
+            animator.SetBool(fallingHash, falling);
         }
 
         float timeToLand = myPlayerMovement.controller.collisions.distanceToFloor / Mathf.Abs(myPlayerMovement.currentVel.y);
         //Debug.LogWarning("vel.y = " + playerMovement.currentVel.y + "; below = " + playerMovement.controller.collisions.below + "; distance to floor = " + playerMovement.controller.collisions.distanceToFloor + "; timeToLand = " + timeToLand);
         if ((myPlayerMovement.currentVel.y < 0 && !myPlayerMovement.controller.collisions.below && timeToLand <= maxTimeToLand)
-            || (jumpingValue && myPlayerMovement.controller.collisions.below))
+            || (falling && myPlayerMovement.controller.collisions.below))
         {
-            if (jumpingValue)
+            if (falling)
             {
-                jumpingValue = false;
-                animator.SetBool(jumpingHash, jumpingValue);
+                falling = false;
+                animator.SetBool(fallingHash, falling);
             }
             //print("SET TRIGGER LAND ");
-            landBool = true;
-            animator.SetBool(landHash, landBool);
+            ground = true;
+            animator.SetBool(groundHash, ground);
         }
 
         if (myPlayerMovement.inWater && myPlayerMovement.currentSpeed > 0)
         {
-            if (swimmingIdle)
+            if (swimming)
             {
-                swimmingIdle = false;
-                animator.SetBool(swimmingIdleHash, swimmingIdle);
+                swimming = false;
+                animator.SetBool(swimmingHash, swimming);
             }
             swimming = true;
             animator.SetBool(swimmingHash, swimming);
@@ -167,20 +261,20 @@ public class PlayerAnimation_01 : MonoBehaviour
                 swimming = false;
                 animator.SetBool(swimmingHash, swimming);
             }
-            swimmingIdle = true;
-            animator.SetBool(swimmingIdleHash, swimmingIdle);
+            swimming = true;
+            animator.SetBool(swimmingHash, swimming);
         }
-        //COMBAT ANIMATIONS
-        if (!basicSwingValue && myPlayerCombat.attackStg == AttackStage.startup)
-        {
-            basicSwingValue = true;
-            animator.SetBool(basicSwingHash, basicSwingValue);
-        }
+        //////COMBAT ANIMATIONS
+        ////if (!basicSwingValue && myPlayerCombat.attackStg == AttackStage.startup)
+        ////{
+        ////    basicSwingValue = true;
+        ////    animator.SetBool(basicSwingHash, basicSwingValue);
+        ////}
     }
 
     public void SetJump(bool _jump)
     {
-        jump = _jump;
-        animator.SetBool(jumpHash, jump);
+        startJump = _jump;
+        animator.SetBool(startJumpHash, startJump);
     }
 }
